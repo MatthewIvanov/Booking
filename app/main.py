00003a8d351fa898f8fa2cc92243d4109ffcd4a9
mat <you@example.com>
2024-10-23 14:52:54 +0300

    add table hotels

--- app/main.py
+++ app/main.py
@@ -1,9 +1,45 @@
-from fastapi import FastAPI
+from datetime import date
+from typing import Optional
+from fastapi import Depends, FastAPI, Query
+from pydantic import BaseModel
 
 app=FastAPI()
 
 
 
-@app.get('/hotels/{id}')
-def get_hotels(id:int,date_from,date_to):
-    return id,date_from,date_to
+class HotelSearchArgs:
+     def __init__(
+               self,
+                location:str,
+                date_from:date,
+                date_to:date,
+                has_spa:Optional[bool] = None,
+                stars:Optional[int]=Query(None,ge=1,le=5)
+            ):
+               self.location=location
+               self.date_from=date_from
+               self.date_to=date_to
+               self.has_spa=has_spa
+               self.stars=stars
+
+class SHotels(BaseModel):
+    address:str
+    name:str
+    stars:int
+
+@app.get('/hotels')
+def get_hotels(
+    search_args:HotelSearchArgs=Depends()
+    ):
+       return search_args
+
+
+
+class SBooking(BaseModel):
+    room_id:int
+    date_from:int
+    date_to:int
+    
+@app.post('/booking')
+def add_booking(booking:SBooking):
+    pass
