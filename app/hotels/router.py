c49bd164b809f61f61ca845550805194fe6632e9
MatthewIvanov <maranafa274@gmail.com>
2024-10-28 21:59:08 +0300

    add some front черноввик

---
+++ app/hotels/router.py
@@ -0,0 +1,29 @@
+
+
+
+from datetime import date, datetime
+from fastapi import APIRouter, Query
+
+from app.main import SHotels
+router= APIRouter(
+    prefix='hotels',
+    tags=['Отели']
+)
+
+# @router.get('')
+# async def get_hotels_by_location_and_time(
+#     location:str,
+#     date_from:date=Query(...,description=f'Например, {datetime.now().date()}'),
+#     date_to:date = Query(...,description=f'Например, {datetime.now().date()}')
+# )->list[HotelInfo]:
+#     hotels: await HotelDAO.search_for_hotels(location,date_from,date_to)
+#     return hotels
+
+# @router.get('/{hotel_id}/rooms')
+# async def get_rooms_by_time(
+#     hotel_id:int,
+#     date_from:date=Query(...,description=f'Например, {datetime.now().date()}'),
+#     date_to:date=Query(...,description=f'Например, {datetime.now().date()}'),
+# )->List[RoomInfo]:
+#     rooms=await HotelDAO.search_for_rooms(hotel_id,date_from,date_to)
+#     return rooms
--- app/main.py
+++ app/main.py
@@ -6,10 +6,13 @@ from pydantic import BaseModel
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
-
+from app.hotels.router import router as router_hotels
+from app.pages.router import router as router_pages
 app=FastAPI()
 app.include_router(router_users)
 app.include_router(router_bookings)
+app.include_router(router_hotels)
+app.include_router(router_pages)
 
 class HotelSearchArgs:
      def __init__(
